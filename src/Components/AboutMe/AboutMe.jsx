ad7ec97174841b0ce505b0c3bec246b8ac2992c8
Gergő Zagyi <70701118+KaaronZ@users.noreply.github.com>
2024-09-11 13:01:19 +0200

    Add files via upload

---
+++ src/Components/AboutMe/AboutMe.jsx
@@ -0,0 +1,10 @@
+import "./AboutMe.css"
+
+const AboutMe = () =>{
+    return (
+    <div className="mainAboutMe">
+        This is About Me
+    </div>
+)
+}
+export default AboutMe
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import "./Navbar.css"
+import PlaceholderImage from '../../Resources/Images/PlaceholderImage.png';
+
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faFacebook } from '@fortawesome/free-brands-svg-icons';
+
+const Navbar = ({selectedPage, setSelectedPage}) => {
+  return (
+    <nav>
+      <div className="profile">
+        <img src={PlaceholderImage} alt="Zagyi Gergő" className="profile-pic" />
+        <h2 className="name">Zagyi Gergő</h2>
+      </div>
+      <ul className="menu">
+        <li
+          onClick={()=> setSelectedPage('Home')}
+        >
+          HOME
+        </li>
+        <li
+          onClick={()=> setSelectedPage('About Me')}
+        >
+          ABOUT ME
+        </li>
+        <li>CREDENTIALS</li>
+        <li>CONTACT </li>
+      </ul>
+      <div className="icons">
+      <FontAwesomeIcon icon={faEnvelope} />
+      <FontAwesomeIcon icon={faLinkedin}/>
+      <FontAwesomeIcon icon={faFacebook}/>
+      <FontAwesomeIcon/>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
